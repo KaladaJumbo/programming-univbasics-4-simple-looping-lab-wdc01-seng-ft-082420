a1382125f34e0337c76fd024e50dfdb69b3c713b
lively-system-6174 <wkalada1@gmail.com>
2020-08-07 02:27:05 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,5 +11,8 @@ def output_array(array)
   array.each { |each| puts each }
 end
 
-def 
+def return_string_array(array)
+  string_array = Array.new
+  array.each {|each| string_array << #{each}
+end
   
