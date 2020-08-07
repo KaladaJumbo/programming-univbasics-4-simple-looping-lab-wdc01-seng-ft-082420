651e7deeac8c4fed87022c00b0bf13cc3c9d1d25
lively-system-6174 <wkalada1@gmail.com>
2020-08-07 02:41:45 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+def loop_message_five_times(message )
+  5.times { puts message }
+end
+
+def loop_message_n_times(message, number)
+  number.times { puts message }
+end
+
+def output_array(array)
+  array.each { |each| puts each }
+end
+
+def return_string_array(array)
+  string_array = Array.new
+  array.each {|each| string_array << "#{each}" }
+  string_array
+end
+  
